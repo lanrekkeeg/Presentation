98a86eb1a1b8522c17dbe493eb9c8e6318b31cf0
Faisal <p156058@nu.edu.pk>
2017-11-30 10:13:32 +0500

    Add files via upload

---
+++ OpeCV sample code/blurr.py
@@ -0,0 +1,13 @@
+# Image Blending
+
+import cv2
+
+img1 = cv2.imread('img_1.jpg')
+img2 = cv2.imread('img_2.jpg')
+dst = cv2.addWeighted(img1,0.5,img2,0.5,0)
+cv2.imshow('Image1',img1)
+cv2.imshow('Image2',img2)
+cv2.imshow('dst',dst)
+cv2.imwrite("blend_image.jpg",dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
